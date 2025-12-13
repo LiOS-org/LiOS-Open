aa35a886b2b62a8852321df2cea7c0e0eec35e07
MD-Saifullah-1524 <muhammadsaifullah2223@gmail.com>
2025-12-13 20:03:08 +0530

    module(constructLiosPopup): Same as liosPopup but constructs popup on go and deletes itself upon closing, no need to write whole lios-popup code just define the `content` and pass it

--- public/modules/JS/liosOpen.js
+++ public/modules/JS/liosOpen.js
@@ -12,3 +12,32 @@ export function liosPopup(action, identifier) {
     console.log("LiOS-Open: module error, unsupported action on LiOS-Popup module");
   };
 };
+export async function constructLiosPopup(content, frosted) {
+  const randomId = "lios-pop-up-" + crypto.randomUUID();
+  let popupContainer;
+  await async function () {
+    popupContainer = document.createElement("div");
+    popupContainer.classList.add("lios-pop-up", "pop-up-is-opening");
+    if (frosted === true) {
+      popupContainer.classList.add("frosted_background");
+    };
+    popupContainer.id = randomId;
+    popupContainer.innerHTML = content;
+    document.body.appendChild(popupContainer);
+  }();
+
+  function close() {
+    liosPopup("close", `#${randomId}`);
+    document.body.removeChild(document.body.querySelector(`#${randomId}`));
+  }
+  document.body.querySelector(`#${randomId}`).style.display = "flex";
+  if (document.querySelector(`#${randomId} .lios-constructed-popup-close`)) {
+    document.querySelector(`#${randomId} .lios-constructed-popup-close`).addEventListener("click", () => {
+      close();
+    });
+  };
+  return {
+    close,
+    element: popupContainer
+  };
+};
