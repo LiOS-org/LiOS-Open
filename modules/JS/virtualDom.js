4af9f02ddb4c3376de0c255355c8dc4ef444e63c
MD-Saifullah-1524 <muhammadsaifullah2223@gmail.com>
2026-03-02 18:53:46 +0530

    feature(implemented update() in `virtualDom`)

--- modules/JS/virtualDom.js
+++ modules/JS/virtualDom.js
@@ -8,32 +8,54 @@ let childrenMethods;
 export const virtualDom = {
     new: async (tag = "div") => {
         let vDom = [];
+        let isMounted = false;
+        let updatableNodes = new Set();
     
         let containerTag = tag;
         let dom;
         let id;
         let classList = [];
         let multiState = {};
-        const generateRandomUid = async () => {
+        let rootPendingUpdates = { newChildrens: [] };
+        let activeStateName = null;
+        const generateRandomUid=()=>{
             const cryptoUid = crypto.randomUUID();
             const randomUid = `virtualDom-${cryptoUid}`;
             return randomUid;
         };
-        const appendTo = (parent, node) => {
-            const targetVDom = node.__state__
-                ? multiState[node.__state__].vDom
+        const appendTo = (parent, node, stateName) => {
+            const targetStateName = stateName || activeStateName;
+            const targetVDom = targetStateName && multiState[targetStateName]
+                ? multiState[targetStateName].vDom
                 : vDom;
-
+            
             if (parent === "root") {
                 targetVDom.push(node);
+                
+                if (isMounted) {
+                    if (targetStateName && multiState[targetStateName]) {
+                        multiState[targetStateName].pendingRootChildrens.push(node);
+                    } else {
+                        rootPendingUpdates.newChildrens.push(node);
+                    }
+                }
+
                 return;
             }
 
             if (parent && parent.__node__) {
-                parent.__node__.children.push(node);
+                const parentNode = parent.__node__;
+                parentNode.children.push(node);
+                if (isMounted) {
+                    if (!parentNode.pendingUpdates.pendingChildrens) {
+                        parentNode.pendingUpdates.pendingChildrens = [];
+                    }
+                    parentNode.pendingUpdates.pendingChildrens.push(node);
+                    updatableNodes.add(parentNode);
+                }
             }
         };
-        const newChild = async (tag = "div") => {
+        const newChild = async (tag = "div", defaultStateName) => {
             const node = {};
             node.children = [];
             node.tag = tag;
@@ -42,14 +64,27 @@ export const virtualDom = {
                 id: "",
                 attributes: {}
             };
+            node.pendingUpdates = {
+                eventListners: {
+                    add: {}
+                }
+            };
             childrenMethods = {
                 classList: (...classList) => {
                     classList.forEach(className => {
                         node.props.class.push(className);
                     });
+                    if (isMounted) {
+                        node.pendingUpdates.classList = classList;
+                        updatableNodes.add(node);
+                    }
                 },
                 id: (id) => {
                     node.props.id = id;
+                    if (isMounted) {
+                        node.pendingUpdates.id = id;
+                        updatableNodes.add(node);
+                    };
                 },
                 attributes: (attributes) => {
                     Object.entries(attributes).forEach(([key, value]) => {
@@ -60,15 +95,36 @@ export const virtualDom = {
                             node.props.attributes[key] = JSON.stringify(value);
                         } else {
                             node.props.attributes[key] = String(value);
-                        }
+                        };
+                        
+                        if (isMounted) {
+                            node.pendingUpdates.attributes = attributes;
+                            updatableNodes.add(node);
+                        };
                     })
                 },
                 textContent: (text) => {
                     node.textContent = text;
+                    
+                    if (isMounted) {
+                        node.pendingUpdates.textContent = text;
+                        updatableNodes.add(node);
+                    };
                 },
-                appendTo: (parent, stateName) => appendTo(parent, node, stateName),
+                appendTo: (parent, stateName) => appendTo(parent, node, stateName || defaultStateName),
                 eventListner: {
-                    add:(type, handler) => vDomEventListener.attach(node,type,handler)
+                    add: (type, handler) => {
+                        vDomEventListener.attach(node, type, handler);
+
+                        if (isMounted) {
+                            node.pendingUpdates.eventListners.add[type] = handler;
+
+                            updatableNodes.add(node);
+                        };
+                    }
+                },
+                get dom() {
+                    return node.dom;
                 },
                 __node__: node
             };
@@ -118,6 +174,10 @@ export const virtualDom = {
                 };
             };
 
+            // Save referance
+            node.dom = el;
+            // 
+
             return el;
         };
         return {
@@ -135,6 +195,9 @@ export const virtualDom = {
                 id = idString;
             },
             render: () => {
+                if (isMounted) {
+                    throw new Error("VirtualDOM already rendered, Use update() for further patching or to rebuild use render({force:true})\n \n To avoid this error it is recommended to render() iff the whole virtualDOM is already created.\n");
+                }
                 const newNode = document.createElement(containerTag);
 
                 if (id && id.length > 0) {
@@ -158,36 +221,124 @@ export const virtualDom = {
 
                 dom.innerHTML = "";
                 dom.replaceChildren(...newNode.childNodes);
+                isMounted = true;
             },
             enableMultiState: () => {
+                activeStateName = "default";
                 multiState.default = {
                     vDom: vDom,
-                    tag:containerTag
+                    tag: containerTag,
+                    pendingRootChildrens: []
                 };
                 return {
                     newState: (stateName, tag = "div") => {
                         const state = stateName;
                         multiState[stateName] = {
                             vDom: [],
-                            tag: tag
+                            tag: tag,
+                            pendingRootChildrens: []
                         };
                         return {
                             newChild: (tag = "div", stateName = state) => newChild(tag, stateName)
                         }
                     },
-                    switchState: async(stateName) => {
+                    switchState: async (stateName) => {
+                        activeStateName = stateName;
                         vDom = multiState[stateName].vDom;
                         containerTag = multiState[stateName].tag;
+
+                        if (isMounted) {
+                            updatableNodes.clear();
+                            multiState[stateName].pendingRootChildrens = [];
+                            const newNode = document.createElement(containerTag);
+                            vDom.forEach(node => {
+                                newNode.appendChild(renderNode(node));
+                            });
+                            dom.innerHTML = "";
+                            dom.replaceChildren(...newNode.childNodes);
+                        }
                     }
                 };
             },
+            update: () => {
+                if (!isMounted) {
+                    throw new Error("VirtualDOM needs to be rendered first");
+                };
+                for (const node of updatableNodes) {
+                    const el = node.dom;
+                    if (!el) {
+                        // Node has not been mounted yet; renderNode will apply props when attached.
+                        node.pendingUpdates = { eventListners: { add: {} } };
+                        continue;
+                    }
+
+                    if (node.pendingUpdates.id) {
+                        el.id = node.pendingUpdates.id;
+                    };
+                    
+                    if (node.pendingUpdates.classList) {
+                        const classes = node.pendingUpdates.classList;
+                        
+                        classes.forEach((className) => {
+                            el.classList.add(className);
+                        });
+                    };
+                    if (node.pendingUpdates.attributes) {
+                        Object.entries(node.pendingUpdates.attributes).forEach(([key, value]) => {
+                            el.setAttribute(key, value);
+                        });
+                    };
+                    if (node.pendingUpdates.textContent) {
+                        el.textContent = node.pendingUpdates.textContent;
+                    };
+                    if (node.pendingUpdates.eventListners?.add) {
+                        for (const [type, handler] of Object.entries(node.pendingUpdates.eventListners.add)) {
+                            attachEventListener(el, type, handler);
+                        };
+                    }
+                    if (node.pendingUpdates.pendingChildrens) {
+                        node.pendingUpdates.pendingChildrens.forEach(childNode => {
+                            el.appendChild(renderNode(childNode));
+                        });
+                    }
+                    node.pendingUpdates = { eventListners: { add: {} } };
+                };
+
+                updatableNodes.clear();
+
+                // Render and append root pending children
+                const pendingList = activeStateName && multiState[activeStateName]
+                    ? multiState[activeStateName].pendingRootChildrens
+                    : rootPendingUpdates.newChildrens;
+                if (pendingList.length > 0) {
+                    pendingList.forEach(node => {
+                        dom.appendChild(renderNode(node));
+                    });
+                    pendingList.length = 0;
+                }
+            },
             eventListner: {
-                attach:(type,handler) => vDomEventListener.attach(vDom,type,handler)
+                attach: (type, handler) => vDomEventListener.attach(vDom, type, handler)
             },
             export: {
                 log: () => {
                     console.log("Exporting virtual DOM structure:");
                     console.log(JSON.stringify(vDom, null, 2));
+                },
+                save: (filename = "virtualDom.json") => {
+                    const json = JSON.stringify(vDom, null, 2);
+
+                    const blob = new Blob([json], { type: "application/json" });
+                    const url = URL.createObjectURL(blob);
+
+                    const a = document.createElement("a");
+                    a.href = url;
+                    a.download = filename;
+                    document.body.appendChild(a);
+                    a.click();
+
+                    document.body.removeChild(a);
+                    URL.revokeObjectURL(url);
                 }
             }
         };
